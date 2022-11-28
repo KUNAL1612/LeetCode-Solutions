ab405538cbf9879fcb7f2ba9c12c64dab465a1b9
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2022-11-27 23:34:56 -0500

    Time: 49 ms (59.54%), Space: 13.7 MB (42.67%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        
+        unordered_map<int,vector<pair<int,int>>> connections;
+        
+        for(auto flight:flights){
+            connections[flight[1]].push_back({flight[0], flight[2]});
+        }
+        vector<vector<int>> dp(k+2, vector<int> (n,INT_MAX));
+        dp[0][src] = 0; //beginning
+        for(int i=1;i<=k+1;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                for(auto edge:connections[j]){
+                    
+                    if(dp[i-1][edge.first]!=INT_MAX){
+                        
+                        // we can go to the src with one less edge
+                        dp[i][j] = min(dp[i][j] ,dp[i-1][edge.first] + edge.second);
+                    }
+                }
+                
+                dp[i][j] = min(dp[i-1][j], dp[i][j]);
+                
+            }
+             
+        }
+        return dp[k+1][dst] < INT_MAX? dp[k+1][dst] : -1;
+        
+    }
+};
