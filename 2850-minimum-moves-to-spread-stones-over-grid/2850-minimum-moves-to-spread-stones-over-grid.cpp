0a81873373245d324e1887f53c8854ebd8bd2cd0
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2023-11-29 19:58:17 -0800

    Time: 860 ms (27.31%), Space: 19 MB (87.74%) - LeetHub

---
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minimumMoves(vector<vector<int>>& grid) {
+        // Base Case
+        bool flag=true;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(grid[i][j]!=1) flag=false;
+            }
+        }
+        if(flag) return 0;
+        int ans=INT_MAX;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int k=0;
+                if(grid[i][j]==0){
+                    for(int a=0;a<3;a++)
+                    {
+                        for(int b=0;b<3;b++){
+                            if(grid[a][b]>1){
+                                grid[a][b]--;
+                                grid[i][j]++;
+                                k+=abs(i-a);
+                                k+=abs(j-b);
+                                ans=min(ans,k+minimumMoves(grid));
+                                grid[a][b]++;
+                                grid[i][j]--;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        return ans;
+    }
+};
