43dac451220ca007bb2a5522c8949acb8a4baf09
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-02-07 01:21:24 -0800

    Time: 4 ms (30.43%), Space: 9.3 MB (21.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int last_even,last_odd;
+        // int last_even,last_odd;
         if(n==0){
             return 0;
         }
@@ -11,13 +11,13 @@ public:
         }
         vector<int> dp(n,0);
         dp[0]=nums[0];
-        dp[1]=nums[1];
+        dp[1]=max(nums[1], dp[0]);
         for(int i=2;i<n;i++){
-            int maex=0;
-            for(int j=0;j<i-1;j++){
-                maex=max(maex, dp[j]+nums[i]);
-            }
-            dp[i]=maex;
+            int max_loot_at_i=0;
+            // for(int j=0;j<i-1;j++){
+            max_loot_at_i=max(dp[i-1], dp[i-2]+nums[i]);
+            // }
+            dp[i]=max_loot_at_i;
         }
         
         return *max_element(dp.begin(),dp.end());
