2498289dee222dd4fe2de456df5104674793af86
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-02-08 10:05:46 -0800

    Time: 4 ms (99.26%), Space: 15 MB (36.56%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return 0;
+        
+        vector<int> rooms;
+        bool foundRoom = false;
+        
+        sort(intervals.begin(), intervals.end());
+        rooms.push_back(intervals[0][1]);
+        
+        for (int i = 1; i < intervals.size(); i++) {
+            for (int j = 0; j < rooms.size(); j++) {
+                if (intervals[i][0] >= rooms[j]) {
+                    rooms[j] = intervals[i][1];
+                    foundRoom = true;
+                    break;
+                }
+            }
+            if (!foundRoom) rooms.push_back(intervals[i][1]);
+            foundRoom = false;
+        }
+        
+        return rooms.size();
+    }
+};
