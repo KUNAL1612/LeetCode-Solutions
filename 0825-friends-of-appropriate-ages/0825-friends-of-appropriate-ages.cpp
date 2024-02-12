68b188101f48d78cb70061915f1647162b46204a
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-02-11 17:53:01 -0800

    Time: 38 ms (72.71%), Space: 39.8 MB (32.69%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -21,61 +21,61 @@
 // };
 
 
-class Solution {
-public:
-    int numFriendRequests(std::vector<int>& ages) {
-        int requests = 0;
-        std::unordered_map<int, int> ageCounts;
-        for (int age : ages) {
-            ageCounts[age]++;
-        }
+// class Solution {
+// public:
+//     int numFriendRequests(std::vector<int>& ages) {
+//         int requests = 0;
+//         std::unordered_map<int, int> ageCounts;
+//         for (int age : ages) {
+//             ageCounts[age]++;
+//         }
 
-        std::vector<int> ageCountSums(121, 0);
-        for (int i = 1; i <= 120; ++i) {
-            ageCountSums[i] = ageCountSums[i - 1] + ageCounts[i]; // cumulative sum
-        }
+//         std::vector<int> ageCountSums(121, 0);
+//         for (int i = 1; i <= 120; ++i) {
+//             ageCountSums[i] = ageCountSums[i - 1] + ageCounts[i]; // cumulative sum
+//         }
 
-        for (int i = 15; i <= 120; ++i) {
-            requests += (ageCounts[i] * (ageCountSums[i - 1] - ageCountSums[i / 2 + 7]) 
-                        + ageCounts[i] * (ageCounts[i] - 1));
-        }
+//         for (int i = 15; i <= 120; ++i) {
+//             requests += (ageCounts[i] * (ageCountSums[i - 1] - ageCountSums[i / 2 + 7]) 
+//                         + ageCounts[i] * (ageCounts[i] - 1));
+//         }
         
-        return requests;
-    }
-};
+//         return requests;
+//     }
+// };
 
 
 
 // I guess in theory the following is constant time, so is at least objectively better. But in a weird scenario where the ages have a greater range than 1 to 121, it would not scale well
 
-//class Solution {
-// public:
-//     bool isPossible(int x,int y) {
-//         if((y <= 0.5*x + 7) || (y > x) || (y > 100 && x < 100))
-//             return false;
-//         return true;
-//     }
-//     int numFriendRequests(vector<int>& ages) {
-//         vector<int> mp(121,0);
-//         for(int i = 0;i < ages.size();i++)
-//             mp[ages[i]]++;
+class Solution {
+public:
+    bool isPossible(int x,int y) {
+        if((y <= 0.5*x + 7) || (y > x) || (y > 100 && x < 100))
+            return false;
+        return true;
+    }
+    int numFriendRequests(vector<int>& ages) {
+        vector<int> mp(121,0);
+        for(int i = 0;i < ages.size();i++)
+            mp[ages[i]]++;
 
-//         int ans = 0;
-//         for(int i = 1;i < 121;i++) {
-//             if(mp[i] == 0)
-//                 continue;
-//             for(int j = 1;j < 121;j++) {
-//                 if(isPossible(i,j)) {
-//                     if(i == j)
-//                         ans += mp[i]*(mp[i] - 1); // if they are the same then just multiply with i-1
-//                     else
-//                         ans += mp[i]*mp[j];
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
+        int ans = 0;
+        for(int i = 1;i < 121;i++) {
+            if(mp[i] == 0)
+                continue;
+            for(int j = 1;j < 121;j++) {
+                if(isPossible(i,j)) {
+                    if(i == j)
+                        ans += mp[i]*(mp[i] - 1); // if they are the same then just multiply with i-1
+                    else
+                        ans += mp[i]*mp[j];
+                }
+            }
+        }
+        return ans;
+    }
+};
 
 
 
