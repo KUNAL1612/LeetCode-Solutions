75384a55381dc59af6df66a284086b551a267f46
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-02-13 15:08:01 -0800

    Time: 3 ms (70.73%), Space: 11.9 MB (43.35%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,20 +1,34 @@
+// class Solution {
+// public:
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         unordered_set<int> integers;
+//         for (string num : nums) {
+//             integers.insert(stoi(num, 0, 2));
+//         }
+        
+//         int n = nums.size();
+//         for (int num = 0; num <= n; num++) {
+//             if (integers.find(num) == integers.end()) { // maybe a hash to avoid O(n^2) search
+//                 string ans = bitset<16>(num).to_string();
+//                 return ans.substr(16 - n);
+//             }
+//         }
+        
+//         return "";
+//     }
+// };
+
+
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<int> integers;
-        for (string num : nums) {
-            integers.insert(stoi(num, 0, 2));
+        string ans;
+        for (int i = 0; i < nums.size(); i++) {
+            char curr = nums[i][i];
+            ans += curr == '0' ? '1' : '0';
         }
         
-        int n = nums.size();
-        for (int num = 0; num <= n; num++) {
-            if (integers.find(num) == integers.end()) { // maybe a hash to avoid O(n^2) search
-                string ans = bitset<16>(num).to_string();
-                return ans.substr(16 - n);
-            }
-        }
-        
-        return "";
+        return ans;
     }
 };
 
