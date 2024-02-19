5f1f4e44de2aee4f72c7af27e08378fcc4e59fce
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-02-18 17:39:44 -0800

    Time: 101 ms (72.28%), Space: 32.3 MB (67.50%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -4,14 +4,14 @@ public:
         int open = 0;
         for(auto c : s) {
             if(c == ']') {
-                if(open) open--;
-                
+                if(open) open--;     
             } 
             else open++;
         }
-        int ans = (open + 1) / 2; //  so because we don't need to swap out every open bracket and every close bracket. Consider the example below where only one swap will balance both the imbalanced opening brackets. 
+        int ans = (open + 1) / 2; //  so because we don't need to swap out every open bracket and every close bracket. Consider the example below where only one swap will balance both the imbalanced opening brackets.  But for odd number of brackets we need to do more than half
+        // for example if the imbalanced opening brackets are like [[[[ then we only need to do two swaps to make this 
         return ans;
     }
 };
 
-// [ ] [ ] [ [ ] [ ] [ ] ]
+// [ ] [ ] ] ] [ ] [ [ 
