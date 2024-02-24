685a42cfeda7edce3d250bf7abe3e91d8c426bb8
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-02-23 21:23:53 -0800

    Time: 20 ms (88.10%), Space: 21.3 MB (39.57%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -8,37 +8,80 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        deque<ListNode*> q1;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            q1.push_back(temp);
-            temp=temp->next;
+        if (head == nullptr) return;
+
+        // find the middle of linked list [Problem 876]
+        // in 1->2->3->4->5->6 find 4 
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        if(q1.size()==0){
-            return;
+
+        // reverse the second part of the list [Problem 206]
+        // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
+        // reverse the second half in-place
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+        ListNode* tmp;
+        while (curr != nullptr) {
+            tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
         }
-        temp=q1.front();
-        q1.pop_front();
-        int f=1; //1 is take from the back
-        while(!q1.empty()){
-            if(f==1){
-                temp->next=q1.back();
-                temp=temp->next;
-                q1.pop_back();
-                f=0;
-            }
-            else{
-                temp->next=q1.front();
-                temp=temp->next;
-                q1.pop_front();
-                f=1;
-            }
+
+        // merge two sorted linked lists [Problem 21]
+        // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        ListNode* first = head;
+        ListNode* second = prev;
+        while (second->next != nullptr) {
+            tmp = first->next;
+            first->next = second;
+            first = tmp;
+            tmp = second->next;
+            second->next = first;
+            second = tmp;
         }
-        temp->next=NULL;
-        
     }
 };
+
+// class Solution {
+// public:
+//     void reorderList(ListNode* head) {
+//         deque<ListNode*> q1;
+//         ListNode* temp=head;
+//         while(temp!=NULL){
+//             q1.push_back(temp);
+//             temp=temp->next;
+//         }
+//         if(q1.size()==0){
+//             return;
+//         }
+//         temp=q1.front();
+//         q1.pop_front();
+//         int f=1; //1 is take from the back
+//         while(!q1.empty()){
+//             if(f==1){
+//                 temp->next=q1.back();
+//                 temp=temp->next;
+//                 q1.pop_back();
+//                 f=0;
+//             }
+//             else{
+//                 temp->next=q1.front();
+//                 temp=temp->next;
+//                 q1.pop_front();
+//                 f=1;
+//             }
+//         }
+//         temp->next=NULL;
+        
+//     }
+// };
 //IDEA: Put everything in a dequeue, and pop from front and back
