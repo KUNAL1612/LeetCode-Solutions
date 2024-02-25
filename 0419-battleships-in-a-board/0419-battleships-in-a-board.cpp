e106dafebbedd041ac5832e9c5e437c20345bf97
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-02-24 21:40:32 -0800

    Time: 4 ms (81.78%), Space: 11.7 MB (20.04%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void coverall(vector<vector<char>>& board, int i, int j){
+        int m = board.size();
+        int n = board[0].size();
+        
+    }
+    int countBattleships(vector<vector<char>>& board) {
+        int m = board.size(), n = m ? board[0].size() : 0, battleships = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X') {
+                    if ((!i || board[i - 1][j] == '.') && (!j || board[i][j - 1] == '.')) {
+                        battleships++;
+                    }
+                }
+            }
+        }
+        return battleships;
+    }
+};
+
+// basically checking for top left ships and top left only
