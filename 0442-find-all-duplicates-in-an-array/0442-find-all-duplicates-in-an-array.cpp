dbe3dbe290702a2b776cf13b8da615015f36343e
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-03-24 21:23:21 -0700

    Time: 39 ms (76.86%), Space: 35.9 MB (90.94%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+
+        // Use cycle sort to place elements
+        // at corresponding index to value
+        int i = 0;
+        while (i < n) {
+            int correctIdx = nums[i] - 1;
+            if (nums[i] != nums[correctIdx]) {
+                swap(nums[i], nums[correctIdx]);
+            } else {
+                i++;
+            }
+        }
+
+        // Any elements not at the index that corresponds to their value are duplicates
+        vector<int> duplicates;
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                duplicates.push_back(nums[i]);
+            }
+        }
+
+        return duplicates;
+    }
+};
