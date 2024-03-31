18dadba9cc4a4d62d728eb05f65d565d6242b814
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2024-03-31 14:34:55 -0700

    Time: 112 ms (77.43%), Space: 105.5 MB (34.66%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> dailyTemperatures(vector<int>& T) {
         int n = T.size();
         vector<int> answer(n,0);
-        deque<int> dq1;
+        stack<int> dq1;
         for(int curr_day = 0; curr_day < n; curr_day++){
             int curr_temp = T[curr_day];
-            while(!dq1.empty() && T[dq1.back()] < curr_temp){
-                int prev_day = dq1.back();
-                dq1.pop_back();
+            while(!dq1.empty() && T[dq1.top()] < curr_temp){
+                int prev_day = dq1.top();
+                dq1.pop();
                 answer[prev_day] = curr_day - prev_day;
             }
-            dq1.push_back(curr_day);
+            dq1.push(curr_day);
         }
         return answer;
     }
